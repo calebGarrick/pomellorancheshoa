2c71fe90361e4b353e4e4d41a7493099f019df70
Caleb Garrick <caleb.garrick@gmail.com>
2026-01-04 21:38:19 -0500

    downgrade dependencies to 8.3, point to env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+$envPath = getenv('APP_ENV_FILE') ?: __DIR__.'/../.env';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname($envPath), basename($envPath));
+$dotenv->safeLoad();
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
