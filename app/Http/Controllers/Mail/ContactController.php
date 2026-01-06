bf9881e758278d81d42dca08114381793164cef6
Caleb Garrick <caleb.garrick@gmail.com>
2026-01-05 23:24:42 -0500

    debug server mailer

--- app/Http/Controllers/Mail/ContactController.php
+++ app/Http/Controllers/Mail/ContactController.php
@@ -36,6 +36,8 @@ class ContactController extends Controller
             ],);
 
         try {
+            Log::info('Mailer in use: ' . config('mail.default'));
+
             Mail::to('pmhoa@mailmt.com')->send(new HoaContactForm(request()->all()));
             return back()->with('success', 'Your message has been sent successfully!');
         } catch (\Exception $e) {
