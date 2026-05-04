cdfe8fc0ae3e772aef0ee3f8a15a0f66ed03fb25
Caleb Garrick <caleb.garrick@gmail.com>
2026-05-04 11:20:43 -0400

    add lotmap

---
+++ resources/views/lotmap2026.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+    <x-slot:title>
+        Meetings
+    </x-slot:title>
+    <div class="card bg-base-100 border border-base-300 mb-4">
+        <div class="card-body">
+            <div class="card-title font-semibold text-2xl "><div style="max-width:700px; margin:0 auto; font-family:Calibri, Arial, sans-serif; color:#222; line-height:1.6;">
+  <div style="background-color:#1f4e79; color:#fff; padding:18px 24px; border-radius:8px 8px 0 0; font-size:28px; font-weight:bold;">
+    click here  
+    
+      <li><a href="https://www.dropbox.com/scl/fi/srg6fuprfaamlxdloaixr/lotmap050226.png?rlkey=02p0i3inqaw3e57dv0y7amuv4&st=9nvxqww3&dl=0" class="link link-primary" target="_blank" rel="noopener">HOA Use Only</a></li>
+    
+    
+  </div>
+  
+  
+</div>
+           
+               
+            </div>
+        </div>
+    </div>
+</x-layout>
